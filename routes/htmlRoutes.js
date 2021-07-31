5d8b44852326e65b2fdcb9cdfe393d997cf16fce
Jaiar Rose <lfj143@gmail.com>
2021-07-30 17:12:51 -0700

    optimized HTML routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -9,6 +9,14 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
+app.get("/assets/js/index.js", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/assets/js/index.js"));
+});
+
+app.get("/assets/css/styles.css", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/assets/css/styles.css"));
+});
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
